3da13c9672be02c9ce27d4119a9ad76ab613c980
AndrewRodabough <arodabough@hotmail.com>
2025-07-24 20:06:45 -0600

    style changes

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -10,8 +10,7 @@ const About: React.FC = () => {
           <p> Hi, I'm Andrew Rodabough, I live in Provo Utah. Currently I am a senior currently
               pursuing a Bachelors in Computer Science at Brigham Young University.
           </p>
-          <p>In my free time, I enjoy working on personal projects, contributing to open source, and exploring the latest trends in web development.</p>
-          <p>I also enjoy ballroom dancing, art, and Cooking</p>
+          <p>In my free time, I enjoy working on personal projects, ballroom dance, art, and cooking.</p>
           <a href="/about" className="morelink" style={{ marginTop: '1rem', alignSelf: 'center' }}>
             Learn More
           </a>
